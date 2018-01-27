14dfed1b364b347dde9f3be481ac39a73c9a4b77
Daniel Clarke <danclarke88@gmail.com>
2018-01-27 13:03:24 -0500

    Controller will vibrate relative to ghost location

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,41 +8,50 @@ public class PlayerController : MonoBehaviour {
 
     public float speed = 2;
 
-    public float leftVibrate = 3;
-    public float rightVibrate = 3;
+    Transform sensedGhost;
 
     void Update () {
         Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Translate(movement.normalized * speed * Time.deltaTime);
         HandleAnimation();
+
+        if(sensedGhost!= null)
+        {
+            if (sensedGhost.position.x < transform.position.x)
+                GamePad.SetVibration(0, 1, 0);
+            else
+                GamePad.SetVibration(0, 0, 1);
+        }
 	}
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 9) //ghost layer
+        if(collision.gameObject.layer == 11) //ghost sensing range
+        {
+            sensedGhost = collision.transform;
+        }
+        else if (collision.gameObject.layer == 9) //ghost layer
         {
             GameManager.instance.SwitchPlanes();
-            SpookyVibrations();
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.layer == 9) //ghost layer
+        if(collision.gameObject.layer == 11) //ghost sensing range
+        {
+            sensedGhost = null;
+            ClearVibration();
+        }
+        else if (collision.gameObject.layer == 9) //ghost layer
         {
             GameManager.instance.SwitchPlanes();
-            ClearVibration();
         }
     }
 
     void HandleAnimation()
     {
 
-    }
-
-    void SpookyVibrations()
-    {
-        GamePad.SetVibration(0, rightVibrate, leftVibrate);
     }
 
     void ClearVibration()
