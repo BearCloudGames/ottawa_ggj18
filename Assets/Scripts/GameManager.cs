1dfb843ac26f10ec70a5584d40a7fb892c31f3f6
Josh Chartrand <joshuachartrand@hotmail.com>
2018-01-27 11:54:47 -0500

    Added a quick sprite changer when switching planes. Made a game manager script.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	GameObject[] EnvironmentAssets;
+
+	// Use this for initialization
+	void Start () {
+		EnvironmentAssets = GameObject.FindGameObjectsWithTag ("Environment");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void SwitchPlanes () {
+		foreach(GameObject environmentAsset in EnvironmentAssets) {
+			environmentAsset.GetComponent<SpriteChanger> ().SwitchSprite ();
+		}
+	}
+}
---
+++ Assets/Scripts/SpriteChanger.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteChanger : MonoBehaviour {
+
+	public Sprite OtherSprite;
+	Sprite CurrentSprite;
+	SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		CurrentSprite = GetComponent<SpriteRenderer> ().sprite;	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void SwitchSprite() {
+		Sprite tempSprite = spriteRenderer.sprite;
+
+		spriteRenderer.sprite = OtherSprite;
+
+		CurrentSprite = spriteRenderer.sprite;
+		OtherSprite = tempSprite;
+	}
+}
