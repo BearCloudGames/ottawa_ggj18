83c2957f0d5515d88d22b99e8f77192c8b4790d8
KiaKortelainen <kiakortelainen@hotmail.ca>
2018-01-26 21:34:01 -0500

    Kia's Fucking around

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour {
+
+
+	Animator anim;
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			anim.SetBool ("Is_Walking", true);
+		}
+		if (Input.GetKeyDown (KeyCode.S)) {
+			anim.SetBool ("Is_Walking", false);
+		}
+
+
+
+	}
+}
