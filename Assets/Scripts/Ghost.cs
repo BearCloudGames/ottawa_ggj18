6d45c068fb193732a4729611ccb6439358457750
Josh Chartrand <joshuachartrand@hotmail.com>
2018-01-27 14:32:25 -0500

    Added a ghost script that looks for trigger collisions to enable dialogue.

---
+++ Assets/Scripts/Ghost.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghost : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D coll) {
+		print ("working");
+		transform.GetChild (0).gameObject.SetActive (true);
+	}
+}
