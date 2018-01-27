c73385e8ac3fc407b5e01537a6074301a478b619
Daniel Clarke <danclarke88@gmail.com>
2018-01-27 11:46:57 -0500

    Adding vibration libraries

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -1,14 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using XInputDotNetPure;
+
 
 public class PlayerController : MonoBehaviour {
 
     public float speed = 2;
-	
-	// Update is called once per frame
-	void Update () {
+
+    public float leftVibrate = 3;
+    public float rightVibrate = 3;
+
+    private void Start()
+    {
+        Vibration();
+    }
+
+    void Update () {
         Vector2 movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
         transform.Translate(movement.normalized * speed * Time.deltaTime);
+        HandleAnimation();
 	}
+
+    void HandleAnimation()
+    {
+
+    }
+
+    //Placeholder method to show the vibration
+    void Vibration()
+    {
+        GamePad.SetVibration(0, rightVibrate, leftVibrate);
+    }
+
+    private void OnApplicationQuit()
+    {
+        GamePad.SetVibration(0, 0, 0);
+    }
 }
