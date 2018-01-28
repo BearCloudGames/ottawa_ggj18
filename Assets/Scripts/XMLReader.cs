48ed3bb14f538ce93c69380de2109b6f6b916a75
Liam Collins <liamcollins333@gmail.com>
2018-01-27 22:21:38 -0500

    Added XML logic for dialogue

---
+++ Assets/Scripts/XMLReader.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+using System.Xml.Linq;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class XMLReader
+{
+    public XMLReader()
+    {
+
+    }
+
+    public List<string> ReadFile(string filename)
+    {
+        XDocument doc = XDocument.Load(filename);
+        List<XElement> lines = doc.Element("Dialogue").Elements("Line").ToList();
+        List<string> strings = new List<string>();
+        foreach (XElement line in lines)
+        {
+            strings.Add(line.Value);
+        }
+        return strings;
+    }
+
+}
