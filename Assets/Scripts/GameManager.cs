f87cc92152cee13666f54522b3bfd647262c5b33
Josh Chartrand <joshuachartrand@hotmail.com>
2018-01-27 20:57:53 -0500

    Changed the sprite switch script to lerp the alphas between two sprites. Imported some alt trees to test with.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -23,7 +23,9 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (Input.anyKeyDown ) {
+			SwitchPlanes ();
+		}
 	}
 
 	public void SwitchPlanes () {
--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class SpriteChanger : MonoBehaviour {
 
-	public Sprite OtherSprite;
-	Sprite CurrentSprite;
-	SpriteRenderer spriteRenderer;
+//	public Sprite OtherSprite;
+//	Sprite CurrentSprite;
+	SpriteRenderer CurrentSpriteRenderer;
+	SpriteRenderer OtherSpriteRenderer;
 
 	// Use this for initialization
 	void Start () {
-		spriteRenderer = GetComponent<SpriteRenderer> ();
-		CurrentSprite = GetComponent<SpriteRenderer> ().sprite;	
+		CurrentSpriteRenderer = this.gameObject.transform.GetChild (0).transform.GetComponent<SpriteRenderer> ();
+		OtherSpriteRenderer = this.gameObject.transform.GetChild (1).transform.GetComponent<SpriteRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -20,11 +21,25 @@ public class SpriteChanger : MonoBehaviour {
 	}
 
 	public void SwitchSprite() {
-		Sprite tempSprite = spriteRenderer.sprite;
+		SpriteRenderer tempSprite = OtherSpriteRenderer;
 
-		spriteRenderer.sprite = OtherSprite;
+		StartCoroutine (FadeBetween (CurrentSpriteRenderer, CurrentSpriteRenderer.color.a, 0.0f, 3.0f));
+		StartCoroutine (FadeBetween (OtherSpriteRenderer, OtherSpriteRenderer.color.a, 1.0f, 3.0f));
 
-		CurrentSprite = spriteRenderer.sprite;
-		OtherSprite = tempSprite;
+		OtherSpriteRenderer = CurrentSpriteRenderer;
+		CurrentSpriteRenderer = tempSprite;
+	}
+
+	private IEnumerator FadeBetween(SpriteRenderer spriteRenderer, float currentAlpha, float alphaFinish, float time)
+	{
+		float elapsedTime = 0;
+
+		while (elapsedTime < time)
+		{
+			currentAlpha = Mathf.Lerp(currentAlpha, alphaFinish, (elapsedTime / time));
+			spriteRenderer.color = new Color (CurrentSpriteRenderer.color.r, CurrentSpriteRenderer.color.g, CurrentSpriteRenderer.color.b, currentAlpha);
+			elapsedTime += Time.deltaTime;
+			yield return new WaitForEndOfFrame();
+		}
 	}
 }
